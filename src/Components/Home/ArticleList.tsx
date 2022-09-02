604ddca21fbb252eb4a38ccd5b7eb55a6ec8ac36
Antoniel Magalhaes <antoniel2210@gmail.com>
2022-09-02 17:37:25 -0300

    push ArticleList

--- src/Components/Home/ArticleList.tsx
+++ src/Components/Home/ArticleList.tsx
@@ -1,4 +1,4 @@
-const arrArticleList = [
+export const arrArticleList = [
   {
     title: "There ain't no such thing as a free tier",
     urlDomain: "youtube.com",
@@ -22,15 +22,22 @@ const arrArticleList = [
   },
 ];
 export const ArticleList = () => {
-  return arrArticleList.map((article, index) => {
-    return (
-      <ArticleItem
-        title={article.title}
-        urlDomain={article.urlDomain}
-        tags={article.tags}
-      />
-    );
-  });
+  return (
+    <>
+      {arrArticleList.map((article, index) => {
+        return (
+          <>
+            <ArticleItem
+              title={article.title}
+              urlDomain={article.urlDomain}
+              tags={article.tags}
+            />
+            <Divider />
+          </>
+        );
+      })}
+    </>
+  );
 };
 
 const ArticleItem = ({ title, urlDomain, tags }) => {
@@ -46,56 +53,6 @@ const ArticleItem = ({ title, urlDomain, tags }) => {
             return <ArticleItemTag key={index} tag={tag} />;
           })}
         </p>
-        <div className="mt-4 flex items-center">
-          <div className="flex mr-2 text-slate-200 text-sm mr-3">
-            <svg
-              fill="none"
-              viewBox="0 0 24 24"
-              className="w-4 h-4 mr-1"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-              />
-            </svg>
-            <span>12</span>
-          </div>
-          <div className="flex mr-2 text-slate-200 text-sm mr-8">
-            <svg
-              fill="none"
-              viewBox="0 0 24 24"
-              className="w-4 h-4 mr-1"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
-              />
-            </svg>
-            <span>8</span>
-          </div>
-          <div className="flex mr-2 text-slate-200 text-sm mr-4">
-            <svg
-              fill="none"
-              viewBox="0 0 24 24"
-              className="w-4 h-4 mr-1"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
-              />
-            </svg>
-            <span>share</span>
-          </div>
-        </div>
       </div>
     </div>
   );
@@ -104,8 +61,10 @@ const ArticleItem = ({ title, urlDomain, tags }) => {
 const ArticleItemTag = ({ tag }) => (
   <span
     key={tag}
-    className="inline-block px-2 py-1 mr-2 text-xs font-medium leading-6 text-slate-200 bg-slate-900 rounded-full"
+    className="inline-block px-2  mr-2 text-xs font-medium leading-6 text-slate-900  rounded-md bg-slate-400"
   >
     {tag}
   </span>
 );
+
+const Divider = () => <div className="border-t border-slate-200 dark:border-slate-800" />;
