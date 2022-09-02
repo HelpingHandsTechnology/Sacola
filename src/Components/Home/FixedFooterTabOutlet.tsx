a7f5d003933ec6bcd16075ca40bbdcf1aed4b0f1
luisfelipesena <luisfelipesena@gmail.com>
2022-09-02 20:57:24 +0000

    feat: redirects in footer

--- src/Components/Home/FixedFooterTabOutlet.tsx
+++ src/Components/Home/FixedFooterTabOutlet.tsx
@@ -2,9 +2,10 @@ import { AiFillHome, AiOutlineHeart, AiOutlineBell } from "react-icons/ai";
 import { IoPersonOutline } from "react-icons/io5";
 
 import type { IconType } from "react-icons";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 interface ITabOutletItemProps {
-  active: boolean;
   icon: IconType;
   href: string;
 }
@@ -16,10 +17,10 @@ interface ITabOutletItemProps {
 export const FixedTabOutlet = () => {
   return (
     <ul className="z-[1] border-t border-gray bg-slate-900  dark flex fixed bottom-0 left-0 right-0 px-8 py-5 justify-between text-sm font-medium text-center text-gray-500 ">
-      <TabOutletItem active={true} icon={AiFillHome} href="#" />
-      <TabOutletItem active={false} icon={AiOutlineHeart} href="#" />
-      <TabOutletItem active={false} icon={AiOutlineBell} href="#" />
-      <TabOutletItem active={false} icon={IoPersonOutline} href="#" />
+      <TabOutletItem icon={AiFillHome} href="/" />
+      <TabOutletItem icon={AiOutlineHeart} href="/favorites" />
+      <TabOutletItem icon={AiOutlineBell} href="/notifications" />
+      <TabOutletItem icon={IoPersonOutline} href="/profile" />
     </ul>
   );
 };
@@ -27,17 +28,20 @@ export const FixedTabOutlet = () => {
 /**
  * TODO: Adicionar label e icone como prop
  */
-function TabOutletItem({ active, icon: Icon, href }: ITabOutletItemProps) {
+function TabOutletItem({ icon: Icon, href }: ITabOutletItemProps) {
   const classActive =
     "flex text-blue-600 rounded-t-lg border-blue-600 active dark:text-blue-500 dark:border-blue-500 group bg-";
   const classInactive =
     "flex rounded-t-lg border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group";
 
+  const router = useRouter();
+  const active = router.pathname === href;
+
   return (
-    <li className="mr-2">
-      <a href={href}>
+    <li className="mr-2 cursor-pointer">
+      <Link href={href}>
         <Icon size={28} className={active ? classActive : classInactive} />
-      </a>
+      </Link>
     </li>
   );
 }
