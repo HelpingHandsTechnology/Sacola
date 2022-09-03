cef190466a6d710456295da6e358a667c06afcfe
Antoniel Magalhaes <antoniel2210@gmail.com>
2022-09-03 11:05:55 -0300

    fix

--- src/pages/api/article-handler.ts
+++ src/pages/api/article-handler.ts
@@ -4,9 +4,9 @@ import { JSDOM } from "jsdom";
 
 export default async function articleHandler(req, res) {
   const response = await axios.get("https://github.com/mozilla/readability");
-  var doc = new JSDOM(response.data);
-  let reader = new Readability(doc.window.document);
-  let article = reader.parse();
+  const doc = new JSDOM(response.data);
+  const reader = new Readability(doc.window.document);
+  const article = reader.parse();
   console.log(article);
   res.status(200).json({ name: "John Doe" });
 }
