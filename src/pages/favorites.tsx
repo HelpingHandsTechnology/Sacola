ed9f2fe8b3503300b95f0ebc5394a71b93404184
luisfelipesena <luisfelipesena@gmail.com>
2022-09-03 14:59:00 +0000

    fix: loading in favorites

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -12,6 +12,7 @@ const Favorites = () => {
         <div className="flex items-start px-4 py-6 bg-slate-400 animate-pulse h-20" />
         <div className="flex items-start px-4 py-6 bg-slate-400 animate-pulse h-20" />
         <div className="flex items-start px-4 py-6 bg-slate-400 animate-pulse h-20" />
+        <FixedHomeTabOutlet />
       </div>
     );
   }
