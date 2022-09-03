224961a51d0bae8f278e4f4c7fb4e6401f4f01f1
Antoniel Magalhaes <antoniel2210@gmail.com>
2022-09-03 00:17:36 -0300

    feat: Favorites

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -1,10 +1,36 @@
+import { ArticleItem } from "../Components/Home/ArticleList";
 import { FixedHomeTabOutlet } from "../Components/Home/FixedHomeTabOutlet";
+import { trpc } from "../utils/trpc";
 
 const Favorites = () => {
+  const articles = trpc.useQuery(["articles.getAll"], { retry: false });
+  const filteredArticles = articles?.data?.filter(
+    (article) => article.isFavorite === true
+  );
+
+  if (!articles.data) {
+    return <div />;
+  }
+
   return (
     <>
       <main>
         <h1 className="bg-slate-100">Favorites</h1>
+        <ul>
+          {filteredArticles?.map((article) => (
+            <li key={article.id}>
+              {
+                <ArticleItem
+                  title={article.title}
+                  urlDomain={article.urlDomain}
+                  tags={article.tags}
+                  id={article.id}
+                  isFavorite={article.isFavorite}
+                />
+              }
+            </li>
+          ))}
+        </ul>
       </main>
       <FixedHomeTabOutlet />
     </>
