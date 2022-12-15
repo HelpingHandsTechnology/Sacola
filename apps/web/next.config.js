cf02532c301e2a33df5b811e5d966cbab9828b03
Antoniel Magalhaes <antoniel2210@gmail.com>
2022-12-15 01:16:48 -0300

    rnw

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,17 @@
-const withTM = require('next-transpile-modules')(['sacola-trpc']);
+const withTM = require('next-transpile-modules')(['sacola-trpc', 'design']);
 
 /**
  * @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      // Transform all direct `react-native` imports to `react-native-web`
+      'react-native$': 'react-native-web',
+    };
+    config.resolve.extensions = ['.web.js', '.web.jsx', '.web.ts', '.web.tsx', ...config.resolve.extensions];
+    return config;
+  },
   reactStrictMode: true,
   swcMinify: true,
 };
