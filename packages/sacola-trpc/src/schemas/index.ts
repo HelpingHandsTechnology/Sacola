6bfb3deaaff6bc5444d40e4873851b0fe725c541
Antoniel Magalhaes <antoniel2210@gmail.com>
2022-12-17 00:30:32 -0300

    A lil fix in the types that guizinho did messed

--- packages/sacola-trpc/src/schemas/index.ts
+++ packages/sacola-trpc/src/schemas/index.ts
@@ -1,34 +1,37 @@
-import { z } from 'zod';
+import { z, ZodLazy, ZodType } from 'zod';
+import { Article, Tags } from 'prisma/prisma-client';
 
 export const userSchema = z.object({
-    id: z.string().uuid(),
-    name: z.string(),
-    email: z.string(),
-    emailVerified: z.boolean(),
-    password: z.string(),
-    createdAt: z.date(),
-    updatedAt: z.date(),
+  id: z.string().uuid(),
+  name: z.string(),
+  email: z.string(),
+  emailVerified: z.boolean(),
+  password: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
 });
 
-export const articleSchema = z.lazy(() => z.object({
+export const articleSchema: ZodLazy<ZodType<Article>> = z.lazy(() =>
+  z.object({
     id: z.string().uuid(),
     title: z.string(),
     urlDomain: z.string(),
     isFavorite: z.string(),
     userId: z.string(),
-    tags: z.array(tagSchema).optional()
-}));
+    tags: z.array(tagSchema).optional(),
+  }),
+);
 
-export const tagSchema = z.object({
-    id: z.string().uuid(),
-    name: z.string(),
-    article: articleSchema.optional(),
-    articleId: z.string().uuid()
+export const tagSchema: ZodType<Tags> = z.object({
+  id: z.string().uuid(),
+  name: z.string(),
+  article: articleSchema.optional(),
+  articleId: z.string().uuid(),
 });
 
 export const articleUserSchema = z.object({
-    userId: z.string().uuid(),
-    articleId: z.string().uuid(),
-    articles: z.array(articleSchema).optional(),
-    users: z.array(userSchema).optional()
+  userId: z.string().uuid(),
+  articleId: z.string().uuid(),
+  articles: z.array(articleSchema).optional(),
+  users: z.array(userSchema).optional(),
 });
