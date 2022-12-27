8a184d258c431a080c953affc6233b8fef6cd576
Antoniel Magalhaes <antoniel2210@gmail.com>
2022-12-26 23:40:56 -0300

    GoBack AppLayout

--- apps/mobile/src/shared/components/AppLayout.tsx
+++ apps/mobile/src/shared/components/AppLayout.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import clsx from 'clsx';
-import { ComponentBaseP } from '../../../App';
+import { ComponentBaseP, MainStackNavigationP } from '../../../App';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { Show } from './Show';
+import { Null } from './Null';
 
 type AppLayoutP = {
   insetTopClassName?: string;
@@ -15,11 +18,24 @@ export const AppLayout = ({
   xClassName,
 }: AppLayoutP) => {
   const inset = useSafeAreaInsets();
+  const navigation = useNavigation<NavigationProp<MainStackNavigationP>>();
+  const canGoBack = navigation.canGoBack();
   return (
     <>
       <View style={{ paddingTop: inset.top }} className={clsx(insetTopClassName)} />
+      <Show when={canGoBack} renderItem={() => <GoBackAppLayout insetTopClassName={insetTopClassName} />} />
       <View className={clsx('flex-grow', xClassName)}>{children}</View>
       <View style={{ paddingBottom: inset.top }} className={clsx(insetBottomClassName)} />
     </>
+  );
+};
+const GoBackAppLayout = (p: AppLayoutP) => {
+  const navigation = useNavigation<NavigationProp<MainStackNavigationP>>();
+  return (
+    <View className={clsx(p.insetTopClassName, 'p-4')}>
+      <TouchableOpacity className={clsx('w-12 h-8 items-center content-center -mx-4 pl-4')} onPress={navigation.goBack}>
+        <Text className="text-2xl ">←</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
