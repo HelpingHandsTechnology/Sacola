3a50bf1e663306419d5abb0944423407a24c47d5
Antoniel Magalhaes <antoniel2210@gmail.com>
2022-12-27 00:10:03 -0300

    SpaceY

--- apps/mobile/src/shared/components/Null.tsx
+++ apps/mobile/src/shared/components/Null.tsx
@@ -1,1 +1,9 @@
+/**
+ * O componente SpaceY não consegue saber se um elemento filho tem seu retorno igual a Null, por isso
+ * o componente Null se faz necessário, com o componente Null é possível verificar o nome do componente
+ * e se ele é Null a gente consegue saber que o elemento filho não tem retorno, assim evitando de dar um
+ * padding num componente que não precisa.
+ * @see https://github.com/facebook/react/issues/5517
+ */
 export const Null = () => null;
+Null.iSNullComponent = true;
