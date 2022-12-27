5b960feed0f371a30c118a5fe240decf4411508d
Antoniel Magalhaes <antoniel2210@gmail.com>
2022-12-26 23:10:49 -0300

    xClassName to AppLayout

--- apps/mobile/src/shared/components/AppLayout.tsx
+++ apps/mobile/src/shared/components/AppLayout.tsx
@@ -4,17 +4,22 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import clsx from 'clsx';
 import { ComponentBaseP } from '../../../App';
 
-export const AppLayout = ({ children, insetTopClassName = '', insetBottomClassName = '' }: AppLayoutP) => {
-  const inset = useSafeAreaInsets();
-  return (
-    <View className={clsx('flex-grow')}>
-      <View style={{ paddingTop: inset.top }} className={clsx(insetTopClassName)} />
-      <>{children}</>
-      <View style={{ paddingBottom: inset.top }} className={clsx(insetBottomClassName)} />
-    </View>
-  );
-};
 type AppLayoutP = {
   insetTopClassName?: string;
   insetBottomClassName?: string;
 } & ComponentBaseP;
+export const AppLayout = ({
+  children,
+  insetTopClassName = 'bg-gray-500',
+  insetBottomClassName = 'bg-gray-500',
+  xClassName,
+}: AppLayoutP) => {
+  const inset = useSafeAreaInsets();
+  return (
+    <>
+      <View style={{ paddingTop: inset.top }} className={clsx(insetTopClassName)} />
+      <View className={clsx('flex-grow', xClassName)}>{children}</View>
+      <View style={{ paddingBottom: inset.top }} className={clsx(insetBottomClassName)} />
+    </>
+  );
+};
