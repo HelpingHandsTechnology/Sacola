2d392db8564380f203fd94c8f8811610dee81179
Antoniel Magalhaes <antoniel2210@gmail.com>
2022-12-27 21:11:42 -0300

    HomeTopCard

--- apps/mobile/src/feature/Home/Home.tsx
+++ apps/mobile/src/feature/Home/Home.tsx
@@ -1,28 +1,53 @@
 import React from 'react';
-import type { MainStackNavigationP } from '../../../App';
-import { View, Text, FlatList } from 'react-native';
+import type { ComponentBaseP } from '../../../App';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { AppLayout } from '../../shared/components/AppLayout';
 import { dummyArticles } from '../../fixtures/articles';
 import { ArticleCard } from './components/ArticleCard';
 import { GreetingComponent } from './components/GreetingComponent';
+import clsx from 'clsx';
+import { Row } from 'design';
 
 export const Home = () => {
-  const FirstLetterUser = 'A';
   return (
-    <AppLayout>
-      <View className="p-4 bg-black rounded-lg mb-8 content-between items-center flex-row">
-        <GreetingComponent />
-        <View className="w-8 h-8 bg-white rounded-lg items-center justify-center">
-          <Text className="text-xs font-bold text-black">{FirstLetterUser}</Text>
-        </View>
-      </View>
-
+    <AppLayout xClassName="bg-white px-4 pb-4 flex-1">
+      <HomeTopCard />
       <FlatList
         ListHeaderComponent={() => <Text className="text-xl font-bold mb-4">Your Articles</Text>}
-        data={dummyArticles}
+        data={dummyArticles.concat(dummyArticles)}
         renderItem={({ item, index }) => <ArticleCard article={item} xClassName={index === 0 ? undefined : 'mt-4'} />}
         keyExtractor={(item) => item.id}
       />
     </AppLayout>
+  );
+};
+
+const HomeTopCard = () => {
+  return (
+    <View className="p-4 bg-black rounded-lg mb-8 content-between items-center flex-row">
+      <GreetingComponent />
+      <Row>
+        <HomeAddLinkButton xClassName="mr-2" />
+        <HomeFirstLetterUserComponent />
+      </Row>
+    </View>
+  );
+};
+type HomeFirstLetterUserComponentP = ComponentBaseP;
+const HomeFirstLetterUserComponent = (p: HomeFirstLetterUserComponentP) => {
+  const FirstLetterUser = 'A';
+  return (
+    <View className={clsx('w-8 h-8 bg-white rounded-lg items-center justify-center', p.xClassName)}>
+      <Text className="text-xs font-bold text-black">{FirstLetterUser}</Text>
+    </View>
+  );
+};
+
+const HomeAddLinkButton = (p: HomeFirstLetterUserComponentP) => {
+  const FirstLetterUser = '+';
+  return (
+    <TouchableOpacity className={clsx('w-8 h-8 bg-white rounded-lg items-center justify-center', p.xClassName)}>
+      <Text className="text-xs font-bold text-black">{FirstLetterUser}</Text>
+    </TouchableOpacity>
   );
 };
