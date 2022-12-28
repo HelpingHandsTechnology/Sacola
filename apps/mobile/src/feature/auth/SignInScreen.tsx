276cebc069df65a35d2730010907fd2ef89a7e77
Antoniel Magalhaes <antoniel2210@gmail.com>
2022-12-28 13:59:16 -0300

    minimal validations SignInScreen

--- apps/mobile/src/feature/auth/SignInScreen.tsx
+++ apps/mobile/src/feature/auth/SignInScreen.tsx
@@ -6,14 +6,56 @@ import { MainStackNavigationP } from '../../../App';
 import { AppButton } from '../../shared/components/AppButton';
 import { AppLayout } from '../../shared/components/AppLayout';
 import { SpaceY } from '../../shared/components/SpaceY';
+import * as Burnt from 'burnt';
+import { throttle } from '../../utils';
+import { trpc } from '../../lib/trpc';
 
 const navigateFactory = (n: NavigationProp<MainStackNavigationP>) => ({
   toSignUpScreen: () => n.navigate('SignUpScreen'),
   toConfirmCodeScreen: () => n.navigate('ConfirmCodeScreen'),
 });
+const emptyEmailThrottle = throttle(
+  ({ title, message }: { title: string; message: string }) =>
+    Burnt.toast({
+      title,
+      message,
+      preset: 'error',
+      duration: 1.5,
+    }),
+  1500,
+);
 export const SignInScreen = () => {
   const navigation = useNavigation<NavigationProp<MainStackNavigationP>>();
   const navigator = navigateFactory(navigation);
+  const [email, setEmail] = React.useState('some@cu');
+  const { mutate } = trpc.user.signIn.useMutation();
+
+  const handleSubmit = () => {
+    if (!email) {
+      return emptyEmailThrottle({
+        message: 'Please enter your email',
+        title: 'Empty email',
+      });
+    }
+    mutate(
+      {
+        email: email,
+      },
+      {
+        onSuccess: () => {
+          return navigator.toConfirmCodeScreen();
+        },
+        onError: (err) => {
+          Burnt.toast({
+            title: 'Error',
+            message: err.message,
+            preset: 'error',
+            duration: 1.5,
+          });
+        },
+      },
+    );
+  };
   return (
     <AppLayout>
       <SpaceY y={32}>
@@ -22,8 +64,14 @@ export const SignInScreen = () => {
           <Text className="text-xl font-light">Sign in to continue</Text>
         </View>
         <SpaceY xClassName="bg-gray-300 rounded-lg p-8" y={24}>
-          <TextInput placeholder="abcd@xyz.com" placeholderTextColor={'#333'} keyboardType="email-address"></TextInput>
-          <AppButton onPress={navigator.toConfirmCodeScreen}>Send code</AppButton>
+          <TextInput
+            onChangeText={setEmail}
+            value={email}
+            placeholder="abcd@xyz.com"
+            placeholderTextColor={'#222222800'}
+            keyboardType="email-address"
+          ></TextInput>
+          <AppButton onPress={handleSubmit}>Send code</AppButton>
           <SignUpText onPress={navigator.toSignUpScreen} />
         </SpaceY>
       </SpaceY>
