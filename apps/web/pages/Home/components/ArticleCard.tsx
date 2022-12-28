2b3f0dae30b1d86f686a795c8f3592acbceeb5de
Lucas Floriani <lucas.floriane@gmail.com>
2022-12-28 15:16:56 -0300

    refactor: use grid into articles grid to make UX better

--- apps/web/pages/Home/components/ArticleCard.tsx
+++ apps/web/pages/Home/components/ArticleCard.tsx
@@ -1,10 +1,10 @@
-import { Text } from "design";
+import { Text } from 'design';
 import { ArticleDTO } from '../../../fixtures/articles';
 
-export default function ArticleCard({article}: {article: ArticleDTO}) {
+export default function ArticleCard({ article }: { article: ArticleDTO }) {
   if (!article) return null;
   return (
-    <article className="w-full md:w-72 lg:w-64 bg-black rounded-lg p-4 h-60" key={article.id}>
+    <article className="w-full bg-black rounded-lg p-4 h-60" key={article.id}>
       <h3 className="text-2xl text-white font-bold">{article.title}</h3>
       <Text xClassName="text-white text-sm">Created at {IntlDate.format(new Date(article.createdAt))}</Text>
     </article>
--- apps/web/pages/Home/index.tsx
+++ apps/web/pages/Home/index.tsx
@@ -7,11 +7,11 @@ export default function Home() {
   return (
     <>
       <main className="flex w-full min-h-screen flex-col p-2 items-center">
-        <div className="w-3/4 flex flex-col gap-8 flex-1 max-w-6xl">
+        <div className="w-full px-4 flex flex-col gap-8 flex-1 max-w-6xl">
           <Header />
-          <section className="flex flex-wrap gap-4">
+          <section className="grid gap-4 grid-cols-fit-16">
             {dummyArticles.map((article) => (
-              <ArticleCard article={article} key={article.id}/>
+              <ArticleCard article={article} key={article.id} />
             ))}
           </section>
         </div>
