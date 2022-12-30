837ad393a00b8f1b4a1a1a3ced53861cb3286501
Antoniel Magalhaes <antoniel2210@gmail.com>
2022-12-29 22:02:29 -0300

    attempt to see if it will build

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,16 +1,11 @@
 import { NavigationContainer, NavigationProp, NavigatorScreenParams } from '@react-navigation/native';
 import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { Text } from 'design';
 import React from 'react';
-import { Image } from 'react-native';
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { withSpring } from 'react-native-reanimated';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthStackParamList, AuthStackScreen } from './src/feature/auth/AuthStack';
-import { SignInScreen } from './src/feature/auth/SignInScreen';
-import { VerifyCodeScreen } from './src/feature/auth/VerifyCodeScreen';
 import { Home } from './src/feature/Home/Home';
 import { InitialScreen } from './src/feature/InitialScreen';
 import { OnboardingHomeScreen } from './src/feature/onboarding/OnboardingHomeScreen';
