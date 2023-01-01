f476b8429cfdeb73bc45109d802fdec512410b53
Antoniel Magalhaes <antoniel2210@gmail.com>
2023-01-01 18:46:19 -0300

    fix: prisma multiple connections

--- packages/sacola-trpc/src/prisma.ts
+++ packages/sacola-trpc/src/prisma.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// Ref: https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
